4a4fb93590d583e04b088db195d863dd48894514
Sarang Patel <sarangp3010@gmail.com>
2020-11-14 12:57:35 +0530

    Update controller.js

--- sparks/controllers/controller.js
+++ sparks/controllers/controller.js
@@ -2,6 +2,19 @@ var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 const { response } = require('express');
 var swal = require('sweetalert');
+var nodemailer = require('nodemailer');
+var transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
+   auth : {
+       user : 'sarangp3010@gmail.com',
+       pass : 'sarang30'
+   } 
+});
+
+
+
 var connection = mongoose.connect('mongodb://localhost:27017/Bank', {useUnifiedTopology: true, useNewUrlParser: true}, (err)=>{
     if(err){
         console.log("Error:   "+err);
@@ -92,12 +105,25 @@ module.exports = function(app){
                 if (err) throw err;
             });
         });
-        swal("Deleted!", "Your imaginary file has been deleted!", "success");
-        res.redirect('/transaction');
+      
+        var mailOption = {
+            from : 'sarangp3010@gmail.com',
+            to : 'sarangpatel30101998@gmail.com',
+            subject : 'The Sparks Foundation',
+            html : `<h1>${req.body.amount}</h1>Rs is Successfully Transfer to <h1>${req.body.name}</h1> `,  
+        };
+        transporter.sendMail(mailOption, (err, info)=>{
+            if(err){
+                console.log("EMAIL"+err);
+            }
+            else{
+                console.log('SENT  : '+info.response);
+            }
+        });
+            res.redirect('/transaction');
+          
+        
     });
-  
-
-    
 
     app.get('/transaction/view/:id', (req, res)=>{
         Bankdbmodel.find({ _id : req.params.id }, (err, data)=>{
