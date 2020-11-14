000c393bee7dac610b60c54be02964bc77e23202
Sarang Patel <sarangp3010@gmail.com>
2020-11-14 12:54:38 +0530

    Create index.js

---
+++ sparks/index.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var controller = require('./controllers/controller');
+
+
+var app = express();
+
+
+app.set('view engine', 'ejs');
+
+app.use(express.static('./styles'));
+
+
+
+controller(app);
+
+
+app.listen(3000, ()=>{
+    console.log('3000 is Activated');
+});
